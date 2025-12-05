d67856d11504a9b8479a0a187ea56fa750723eb9
nazik <chargynbaevanazik@gmail.com>
2025-12-06 00:23:07 +0600

    style

---
+++ script.js
@@ -0,0 +1,12 @@
+
+ const box_div=()=>{
+    let div=document.getElementById("box")
+   div.style.marginLeft="20px"
+    div.style.marginTop="30px"
+    div.style.borderRadius="20px"
+    div.style.border="2px solid black"
+    div.style.boxShadow="2px 5px 7px"
+    div.style.width="320px"
+    div.style.height="300px"
+    div.style.backgroundImage=`url(https://images.pexels.com/photos/1187079/pexels-photo-1187079.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500)`
+ }
